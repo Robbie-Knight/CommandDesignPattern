9b001f4c81085869c5c16cd76d0d7691dce931ab
Marcos Leon <marcos850@Marcoss-MacBook-Pro-2.local>
2020-10-10 17:58:34 -0400

    made changes to javadoc

--- src/Command.java
+++ src/Command.java
@@ -1,5 +1,10 @@
-
+/**
+ * this interface is to execute the command to either buy or sell the stock
+ */
 public interface Command {
 
+	/**
+	 * this method executes the command
+	 */
 	public void execute();
 }
