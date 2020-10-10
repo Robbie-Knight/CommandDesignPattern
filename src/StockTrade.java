7120f64b2102cb8e34e345415ae23e2499094997
katiekopper <56326272+katiekopper@users.noreply.github.com>
2020-10-10 18:20:27 -0400

    Update StockTrade.java

--- src/StockTrade.java
+++ src/StockTrade.java
@@ -1,12 +1,20 @@
-
+**/ public class StockTrade **/
 public class StockTrade {
 
+	/** public StockTrade is left empty
+	**/
 	public StockTrade() {}
 	
+	/** public void buy has parameters String stockName, int numShares
+	  prints how many shares a user is buying
+	**/
 	public void buy(String stockName, int numShares) {
 		System.out.println("Buying "+ numShares + " of " + stockName);
 	}
-	
+
+	/** public void sell has parameters String stockName, int numShares
+	prints how many shares a user is selling
+	**/
 	public void sell(String stockName, int numShares) {
 		System.out.println("Selling "+ numShares + " of " + stockName);
 	}
