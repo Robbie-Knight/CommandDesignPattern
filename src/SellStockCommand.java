51dc63fc0ed9c7f70675d348a52cabab7a35ae04
Marcos Leon <marcos850@Marcoss-MacBook-Pro-2.local>
2020-10-10 18:10:10 -0400

    made changes to javadoc

--- src/SellStockCommand.java
+++ src/SellStockCommand.java
@@ -1,24 +1,16 @@
-/**
- * this class is used to sell stocks
- */
+
 public class SellStockCommand implements Command{
 
 	private StockTrade stockTrade;
 	private String stockName;
 	private int shares;
 
-	/**
-	 * this method is used to know how many shares of which stock to sell or trade
-	 */
 	public SellStockCommand(StockTrade stockTrade, String stockName, int shares) {
 		this.shares = shares;
 		this.stockName = stockName;
 		this.stockTrade = stockTrade;
 	}
 
-	/**
-	 * this method executes the sell
-	 */
 	public void execute() {
 		stockTrade.sell(stockName, shares);
 	}
